dea0831b71938f375d20f69da3c8a6b061807130
zombiegh0st <8b4ll@gmx.de>
2021-07-08 21:52:00 +0200

    FEATURE root-shape-dice

---
+++ components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+	<button class="btn btn-primary" v-on:click="onClick">{{ content }}</button>
+</template>
+
+<script>
+	export default {
+		props: {
+			content: String,
+			onClick: Function,
+		},
+	};
+</script>
+
+<style></style>
