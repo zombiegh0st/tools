ad861f441366f9f1ff50a6f5ec6e17297a9c238b
AD <8b4ll@gmx.de>
2022-07-22 22:19:21 +0200

    FEATURE Chor-Dice, toene des akkords werden generiert

--- pages/chord-dice.vue
+++ pages/chord-dice.vue
@@ -2,7 +2,10 @@
 	<div class="main container d-flex align-items-center min-vh-100">
 		<div class="col text-center align-middle">
 			<div class="row align-items-center">
-				<span class="result">{{ roots[idxRoot] }}{{ mod[idxShape] }}</span>
+				<span class="resultChord">{{ resultChord }}</span>
+			</div>
+			<div class="row align-items-center">
+				<span class="resultTones">{{ resultTones }}</span>
 			</div>
 			<div>
 				<Button class="roll-button" content="Roll" :onClick="roll" />
@@ -16,9 +19,11 @@
 	export default {
 		data: function () {
 			return {
-				idxRoot: 0,
-				idxShape: 0,
-				mod: ['', 'm'],
+				resultChord: '',
+				resultTones: '',
+				//idxRoot: 0,
+				//idxMod: 0,
+				mods: ['', 'm'],
 				roots: [
 					'C',
 					'C#',
@@ -38,6 +43,8 @@
 					'Bb',
 					'B',
 				],
+				tones_sharp: ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'],
+				tones_flat: ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'],
 			};
 		},
 
@@ -46,15 +53,36 @@
 		},
 
 		methods: {
-			getRandomInt: function (max) {
+			getRandomInt(max) {
 				return Math.floor(Math.random() * max);
 			},
-			roll: function (e) {
-				var newRoot = this.getRandomInt(this.roots.length);
-				this.idxRoot = newRoot;
 
-				var newShape = this.getRandomInt(this.mod.length);
-				this.idxShape = newShape;
+			isFlat(chord) {
+				return chord.slice(-1) === 'b' || chord.slice(-2) === 'bm';
+			},
+
+			isMinor(chord) {
+				return chord.slice(-1) === 'm';
+			},
+
+			getChordTones(chord) {
+				const tones = this.isFlat(chord) ? this.tones_flat : this.tones_sharp;
+				const root = this.isMinor(chord) ? chord.slice(0, -1) : chord;
+				const rootIdx = tones.indexOf(root);
+				const thirdInterval = this.isMinor(chord) ? 3 : 4;
+				const thirdIdx = (rootIdx + thirdInterval) % tones.length;
+				const fifthInterval = 7;
+				const fifthIdx = (rootIdx + fifthInterval) % tones.length;
+				const third = tones[thirdIdx];
+				const fifth = tones[fifthIdx];
+				return `${root}, ${third}, ${fifth}`;
+			},
+
+			roll(e) {
+				var idxRoot = this.getRandomInt(this.roots.length);
+				var idxMod = this.getRandomInt(this.mods.length);
+				this.resultChord = this.roots[idxRoot] + this.mods[idxMod];
+				this.resultTones = this.getChordTones(this.resultChord);
 			},
 		},
 	};
@@ -66,7 +94,7 @@
 		height: 9vh
 		font-size: 4vh
 
-	.result
+	.resultChord
 		color: #333
 		font-size: 6em
 		margin: 1em 0em
